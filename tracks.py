d29a7fdb37d6c03f4f551c3333755542630cf47a
gpborla <gpborla+github@gmail.com>
2018-07-07 21:39:39 +0800

    track script achieved objective

--- tracks.py
+++ tracks.py
@@ -32,8 +32,7 @@ CREATE TABLE Track (
         AUTOINCREMENT UNIQUE,
     title TEXT  UNIQUE,
     album_id  INTEGER,
-    genre_id  INTEGER,
-    len INTEGER, rating INTEGER, count INTEGER
+    genre_id  INTEGER
 );
 ''')
 
@@ -61,28 +60,30 @@ for entry in all:
     name = lookup(entry, 'Name')
     artist = lookup(entry, 'Artist')
     album = lookup(entry, 'Album')
-    count = lookup(entry, 'Play Count')
-    rating = lookup(entry, 'Rating')
-    length = lookup(entry, 'Total Time')
+    genre = lookup(entry, 'Genre')
 
-    if name is None or artist is None or album is None :
+    if name is None or artist is None or album is None or genre is None:
         continue
 
-    print(name, artist, album, count, rating, length)
+    print(name, artist, album, genre)
 
     cur.execute('''INSERT OR IGNORE INTO Artist (name) 
-        VALUES ( ? )''', ( artist, ) )
+        VALUES ( ? )''', (artist, ))
     cur.execute('SELECT id FROM Artist WHERE name = ? ', (artist, ))
     artist_id = cur.fetchone()[0]
 
     cur.execute('''INSERT OR IGNORE INTO Album (title, artist_id) 
-        VALUES ( ?, ? )''', ( album, artist_id ) )
+        VALUES ( ?, ? )''', (album, artist_id))
     cur.execute('SELECT id FROM Album WHERE title = ? ', (album, ))
     album_id = cur.fetchone()[0]
 
+    cur.execute('''INSERT OR IGNORE INTO Genre(name)
+        VALUES(?)''', (genre,))
+    cur.execute('SELECT id FROM Genre WHERE name = ? ', (genre, ))
+    genre_id = cur.fetchone()[0]
+
     cur.execute('''INSERT OR REPLACE INTO Track
-        (title, album_id, len, rating, count) 
-        VALUES ( ?, ?, ?, ?, ? )''',
-        ( name, album_id, length, rating, count ) )
+        (title, album_id, genre_id) 
+        VALUES ( ?, ?, ?)''',(name, album_id, genre_id))
 
     conn.commit()
